4eb7d53c7063011740745efb79dabc13977d5b4a
davidporras31 <43134667+davidporras31@users.noreply.github.com>
2025-10-20 22:34:06 +0200

    add relative transform

--- Transformable.cpp
+++ Transformable.cpp
@@ -1,14 +1,44 @@
 #include "Transformable.h"
 
+Transformable::~Transformable()
+{
+    if(!parent)
+        return;
+    auto it = std::find(parent->children.begin(),parent->children.end(),this);
+    if(it != parent->children.end())
+        parent->children.erase(it);
+        
+}
+
+void Transformable::setParent(Transformable *par)
+{
+    if(par == this)
+        return;
+    parent = par;
+    dirty = true;
+    if(par) par->children.push_back(this);
+}
+
 glm::mat4 &Transformable::getTransform()
 { 
     if (dirty) {
+        if(children.size()!= 0)
+        for (size_t i = 0; i < children.size(); i++)
+        {
+            children[i]->dirty = true;
+        }
+        
+        
         transform = glm::mat4(1.0f);
         transform = glm::translate(transform, position);
         transform = glm::rotate(transform, rotation.x, glm::vec3(1, 0, 0));
         transform = glm::rotate(transform, rotation.y, glm::vec3(0, 1, 0));
         transform = glm::rotate(transform, rotation.z, glm::vec3(0, 0, 1));
         transform = glm::scale(transform, scale);
+        if(parent)
+        {
+            transform = parent->getTransform() * transform; 
+        }
         dirty = false;
     }
     return transform; 
--- Transformable.h
+++ Transformable.h
@@ -2,6 +2,8 @@
 #define TRANSFORMABLE_H
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+#include <algorithm>
 
 class Transformable
 {
@@ -11,14 +13,19 @@ private:
     glm::vec3 scale;
     bool dirty;
     glm::mat4 transform;
+    Transformable* parent;
+    std::vector<Transformable*> children;
 public:
-    Transformable() : position(0.0f, 0.0f, 0.0f), rotation(0.0f, 0.0f, 0.0f), scale(1.0f, 1.0f, 1.0f), dirty(true) {}
+    Transformable() : position(0.0f, 0.0f, 0.0f), rotation(0.0f, 0.0f, 0.0f), scale(1.0f, 1.0f, 1.0f), dirty(true), parent(nullptr) {}
+    ~Transformable();
     void setPosition(const glm::vec3& pos) { position = pos; dirty = true; }
     glm::vec3 getPosition() const { return position; }
     void setRotation(const glm::vec3& rot) { rotation = rot; dirty = true; }
     glm::vec3 getRotation() const { return rotation; }
     void setScale(const glm::vec3& scl) { scale = scl; dirty = true; }
     glm::vec3 getScale() const { return scale; }
+    void setParent(Transformable* par);
+    Transformable* getParent() const { return parent; }
     glm::mat4& getTransform();
 };
 
